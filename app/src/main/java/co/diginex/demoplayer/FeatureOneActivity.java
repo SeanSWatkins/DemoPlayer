84e614b6f62a3783af31c422e724fce678f279e5
Sean Watkins <s.watkins@ranworx.com>
2016-05-11 15:59:07 +0200

    Initial Commit

---
+++ app/src/main/java/co/diginex/demoplayer/FeatureOneActivity.java
@@ -0,0 +1,13 @@
+package co.diginex.demoplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeatureOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feature_one);
+    }
+}
